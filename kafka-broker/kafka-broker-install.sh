82dd599e61612f483e1ce2ff087b6ce6f6d2665d
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-01-29 16:53:45 +0000

    DW-1986
    added zookeeper/kafka service startup files that will run from init.d rc.local on bootup.
    With Systemd, the Init scripts are not there any more. Consequently,
    the execution of tasks at boot time had to change.
    In order to execute tasks at boot time, you put them into the /etc/rc.d/rc.local file.
    Then, you change the execution permissions on this file:
    There are many files and perms that will need to go through lambda/packer mechanism

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -3,11 +3,12 @@
 ## Script to install a kafka & zookeeper broker
 
 # Install kafka & zookeeper
-	sudo useradd kafka -m
-	cd /usr/local
-	mkdir kafka
-	chown kafka:kafka -R /usr/local/kafka
- 	curl "http://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
-	tar -xvzf /tmp/kafka.tgz --strip 1
-    chmod u+x /usr/local/kafka/bin/zookeeper-server-start.sh
-    chmod u+x /usr/local/kafka/bin/kafka-server-start.sh
+sudo useradd kafka -m
+cd /usr/local
+mkdir kafka
+chown kafka:kafka -R /usr/local/kafka
+curl "http://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
+tar -xvzf /tmp/kafka.tgz --strip 1
+chmod u+x /usr/local/kafka/bin/zookeeper-server-start.sh
+chmod u+x /usr/local/kafka/bin/kafka-server-start.sh
+

f3f79e87e8ed2ffcfcdc8ff5b269086da0839366
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-02-05 09:03:07 +0000

    DW-1986
    changed actual script to have /tmp/ami-builder/kafka-broker/ paths

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -5,25 +5,25 @@
 # Install kafka & zookeeper
 sudo useradd kafka -m
 
-cd /usr/local && mkdir kafka
-chown kafka:kafka -R /usr/local/kafka
-curl "http://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
-tar -xvzf /tmp/kafka.tgz --strip 1
-cp kafka-broker/kafka /etc/init.d
-cp kafka-broker/zookeeper /etc/init.d
-yum install -y java-1.8.0-openjdk-devel
-cp zookeeper     /etc/rc.d/init.d
-cp kafka         /etc/rc.d/init.d
-cp server.properties     /usr/local/kafka/config
-touch  /var/log/kafka/zookeeper.out /var/log/kafka/zookeeper.err /var/log/kafka/kafka.out /var/log/kafka/kafka.err
-chmod u+x             /etc/rc.d/init.d/zookeeper
-chmod u+x             /etc/rc.d/init.d/kafka
-chmod u+x             /usr/local/kafka/bin/zookeeper-server-start.sh
-chmod u+x             /usr/local/kafka/bin/kafka-server-start.sh
-chmod u+x             /etc/rc.d/rc.local
-chown kafka:kafka -R  /usr/local/kafka
-chown kafka:kafka -R  /var/log/kafka
-chown kafka:kafka -R  /tmp/zookeeper
-chown kafka:kafka -R  /tmp/kafka-logs
-chkconfig zookeeper on
-chkconfig kafka on
+sudo cd /usr/local && mkdir kafka
+sudo chown kafka:kafka -R /usr/local/kafka
+sudo curl "http://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
+sudo tar -xvzf /tmp/kafka.tgz --strip 1
+sudo cp kafka-broker/kafka /etc/init.d
+sudo cp kafka-broker/zookeeper /etc/init.d
+sudo yum install -y java-1.8.0-openjdk-devel
+sudo cp /tmp/ami-builder/kafka-broker/zookeeper     /etc/rc.d/init.d
+sudo cp /tmp/ami-builder/kafka-broker/kafka         /etc/rc.d/init.d
+sudo cp /tmp/ami-builder/kafka-broker/server.properties     /usr/local/kafka/config
+sudo touch  /var/log/kafka/zookeeper.out /var/log/kafka/zookeeper.err /var/log/kafka/kafka.out /var/log/kafka/kafka.err
+sudo chmod u+x             /etc/rc.d/init.d/zookeeper
+sudo chmod u+x             /etc/rc.d/init.d/kafka
+sudo chmod u+x             /usr/local/kafka/bin/zookeeper-server-start.sh
+sudo chmod u+x             /usr/local/kafka/bin/kafka-server-start.sh
+sudo chmod u+x             /etc/rc.d/rc.local
+sudo chown kafka:kafka -R  /usr/local/kafka
+sudo chown kafka:kafka -R  /var/log/kafka
+sudo chown kafka:kafka -R  /tmp/zookeeper
+sudo chown kafka:kafka -R  /tmp/kafka-logs
+sudo chkconfig zookeeper on
+sudo chkconfig kafka on

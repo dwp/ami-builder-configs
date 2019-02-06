79e6b60533d803aab5c2100e23e913028f6bbbd4
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-02-06 13:53:49 +0000

    changes to startup scripts inc tar extract dir

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -3,8 +3,10 @@
 ## Script to install a kafka & zookeeper broker
 
 # Install Java
-sudo yum --disablerepo=epel\*
-sudo yum clean all
+
+sudo yum clean all -y
+sudo yum update -y
+
 sudo yum install -y java-1.8.0-openjdk-devel
 
 # Install kafka & zookeeper
@@ -12,14 +14,17 @@ sudo useradd kafka -m
 sudo mkdir /usr/local/kafka
 sudo chown kafka:kafka -R /usr/local/kafka
 sudo curl "https://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
-sudo cd /usr/local/kafka
-sudo tar -xvzf /tmp/kafka.tgz --strip 1
+sudo tar -xvzf /tmp/kafka.tgz --strip 1 --directory /usr/local/kafka
 sudo cp /tmp/ami-builder/kafka-broker/zookeeper     /etc/init.d
 sudo cp /tmp/ami-builder/kafka-broker/kafka         /etc/init.d
 sudo cp /tmp/ami-builder/kafka-broker/zookeeper     /etc/rc.d/init.d
 sudo cp /tmp/ami-builder/kafka-broker/kafka         /etc/rc.d/init.d
 sudo cp /tmp/ami-builder/kafka-broker/server.properties     /usr/local/kafka/config
+sudo cp /tmp/ami-builder/kafka-broker/rc.local    /etc/rc.d/rc.local
+sudo mkdir /var/log/kafka
+sudo mkdir /tmp/zookeeper
 sudo touch  /var/log/kafka/zookeeper.out /var/log/kafka/zookeeper.err /var/log/kafka/kafka.out /var/log/kafka/kafka.err
+sudo chmod u+x             /etc/rc.d/rc.local
 sudo chmod u+x             /etc/init.d/zookeeper
 sudo chmod u+x             /etc/init.d/kafka
 sudo chmod u+x             /etc/rc.d/init.d/zookeeper
@@ -30,5 +35,4 @@ sudo chown kafka:kafka -R  /usr/local/kafka
 sudo chown kafka:kafka -R  /var/log/kafka
 sudo chown kafka:kafka -R  /tmp/zookeeper
 sudo chown kafka:kafka -R  /tmp/kafka-logs
-sudo chkconfig zookeeper on
-sudo chkconfig kafka on
+

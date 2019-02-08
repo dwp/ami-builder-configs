33f7ab8faa83f72cc24ba7194d63cebbf968861c
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-02-08 17:31:59 +0000

    more amendments to cp commands as Dir's are wrong from the Lambda

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -13,6 +13,12 @@ sudo mkdir /usr/local/kafka
 sudo chown kafka:kafka -R /usr/local/kafka
 sudo curl "https://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
 sudo tar -xvzf /tmp/kafka.tgz --strip 1 --directory /usr/local/kafka
+sudo cp /tmp/ami-builder/kafka-broker/zookeeper/zookeeper     /etc/init.d
+sudo cp /tmp/ami-builder/kafka-broker/kafka/kafka         /etc/init.d
+sudo cp /tmp/ami-builder/kafka-broker/zookeeper/zookeeper     /etc/rc.d/init.d
+sudo cp /tmp/ami-builder/kafka-broker/kafka/kafka         /etc/rc.d/init.d
+sudo cp /tmp/ami-builder/kafka-broker/server.properties/server.properties     /usr/local/kafka/config
+sudo cp /tmp/ami-builder/kafka-broker/rc.local/rc.local    /etc/rc.d
 sudo mkdir /var/log/kafka
 sudo mkdir /tmp/zookeeper
 sudo touch  /var/log/kafka/zookeeper.out /var/log/kafka/zookeeper.err /var/log/kafka/kafka.out /var/log/kafka/kafka.err

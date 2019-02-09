5667374ebdaf404e8fb61bba41969f4491e0911a
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-02-09 11:26:42 +0000

    creted inline code for provisioners

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -13,8 +13,8 @@ sudo mkdir /usr/local/kafka
 sudo chown kafka:kafka -R /usr/local/kafka
 sudo curl "https://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
 sudo tar -xvzf /tmp/kafka.tgz --strip 1 --directory /usr/local/kafka
-sudo cp tmp/ami-builder/kafka-broker/zookeeper     /etc/init.d
-sudo cp tmp/ami-builder/kafka-broker/kafka         /etc/init.d
+sudo cp ../ami-builder/kafka-broker/zookeeper     /etc/init.d
+sudo cp ../ami-builder/kafka-broker/kafka         /etc/init.d
 sudo cp tmp/ami-builder/kafka-broker/zookeeper     /etc/rc.d/init.d
 sudo cp tmp/ami-builder/kafka-broker/kafka         /etc/rc.d/init.d
 sudo cp tmp/ami-builder/kafka-broker/server.properties     /usr/local/kafka/config

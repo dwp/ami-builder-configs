68a5cc7f08b4ea18037de7f13fb64440434ec906
Marco <marcochessa@digital.uc.dwp.gov.uk>
2019-02-11 12:24:10 +0000

    debugging file creating

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -3,8 +3,8 @@
 ## Script to install a kafka & zookeeper broker
 
 # Install Java
-sudo yum update -y
-sudo yum install -y java-1.8.0-openjdk-devel
+###sudo yum update -y
+####sudo yum install -y java-1.8.0-openjdk-devel
 
 # Install kafka & zookeeper
 sudo useradd kafka -m
@@ -12,27 +12,36 @@ sudo mkdir /usr/local/kafka
 sudo chown kafka:kafka -R /usr/local/kafka
 sudo curl "https://www-eu.apache.org/dist/kafka/1.0.2/kafka_2.12-1.0.2.tgz" -o /tmp/kafka.tgz
 sudo tar -xvzf /tmp/kafka.tgz --strip 1 --directory /usr/local/kafka
+echo " Starting copy zookeeper to /etc/init.d/"
 sudo cp /tmp/ami-builder/kafka-broker/zookeeper     /etc/init.d
+echo " Starting copy kafka to /etc/init.d/"
 sudo cp /tmp/ami-builder/kafka-broker/kafka         /etc/init.d
 sudo cp /tmp/ami-builder/kafka-broker/server.properties     /usr/local/kafka/config
+###REMOVE THIS BLOCK LATER
+###
 #sudo cp /tmp/ami-builder/kafka-broker/zookeeper     /etc/rc.d/init.d
 #sudo cp /tmp/ami-builder/kafka-broker/kafka         /etc/rc.d/init.d
 #sudo cp /tmp/ami-builder/kafka-broker/rc.local     /etc/rc.d
+#sudo chmod u+x             /etc/rc.d/rc.local
+#sudo chmod u+x             /etc/rc.d/init.d/zookeeper
+#sudo chmod u+x             /etc/rc.d/init.d/kafka
+############################
+
 sudo mkdir /var/log/kafka
 sudo mkdir /tmp/kafka-logs
 sudo mkdir /tmp/zookeeper
 sudo touch  /var/log/kafka/zookeeper.out /var/log/kafka/zookeeper.err /var/log/kafka/kafka.out /var/log/kafka/kafka.err
-sudo chmod u+x             /etc/rc.d/rc.local
 sudo chmod u+x             /etc/init.d/zookeeper
 sudo chmod u+x             /etc/init.d/kafka
-sudo chmod u+x             /etc/rc.d/init.d/zookeeper
-sudo chmod u+x             /etc/rc.d/init.d/kafka
 sudo chmod u+x             /usr/local/kafka/bin/zookeeper-server-start.sh
 sudo chmod u+x             /usr/local/kafka/bin/kafka-server-start.sh
 sudo chown kafka:kafka -R  /usr/local/kafka
 sudo chown kafka:kafka -R  /var/log/kafka
 sudo chown kafka:kafka -R  /tmp/zookeeper
 sudo chown kafka:kafka -R  /tmp/kafka-logs
+echo "Printing the head of file /etc/init.d/zookeeper"
+sudo ls -ltr /etc/init.d/zookeeper
+sudo  head -15 /etc/init.d/zookeeper
 sudo chkconfig --add zookeeper
 sudo chkconfig --add kafka
 sudo systemctl start zookeeper

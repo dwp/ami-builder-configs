c30c5dabc7109050af8d169976262d0c61c5339a
Marco <marcochessa@digital.uc.dwp.gov.uk>
2019-02-14 10:34:04 +0000

    Install Amazon SSM agent when creating the AMI - moved the installation to its own block

--- kafka-broker/kafka-broker-install.sh
+++ kafka-broker/kafka-broker-install.sh
@@ -2,9 +2,11 @@
 
 ## Script to install a kafka & zookeeper broker
 
-# Install Java and SSM agent
+# Install Java
 sudo yum update -y
 sudo yum install -y java-1.8.0-openjdk-devel
+
+# Install Amazon SSM agent
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 
 # Install kafka & zookeeper

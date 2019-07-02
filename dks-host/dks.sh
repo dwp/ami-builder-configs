48fa6c24c29a7b5039c7d91e30c931c52955bfd4
Benjamin Sherwood <benjamin.sherwood@dwp.gov.uk>
2019-07-02 16:11:42 +0100

    DW-2404 - Fixed java options so tcp not tcp6

--- dks-host/dks.sh
+++ dks-host/dks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 export SPRING_PROFILES_ACTIVE=AWS,KMS,INSECURE
-java -jar /opt/dks/dks.jar
+java -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -jar /opt/dks/dks.jar

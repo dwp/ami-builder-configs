687a4b22769e4caf5c745d8851ada48d1ce39947
Benjamin Sherwood <benjamin.sherwood@dwp.gov.uk>
2019-07-31 13:42:34 +0100

    Changed log location to be the dks subdir

--- dks-host/dks.sh
+++ dks-host/dks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 export SPRING_PROFILES_ACTIVE=AWS,KMS,INSECURE
-java -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Ddks.log.directory=/var/log -Ddks.log.level.console=WARN -Ddks.log.level.file=INFO -jar /opt/dks/dks.jar
+java -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Ddks.log.directory=/var/log/dks -Ddks.log.level.console=WARN -Ddks.log.level.file=INFO -jar /opt/dks/dks.jar

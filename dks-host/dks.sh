0b11ccc4f10ef88a13b3e83f9125c867d6fd5159
Benjamin Sherwood <benjamin.sherwood@dwp.gov.uk>
2019-08-01 17:17:35 +0100

    DW-2491 - Switch DKS to SECURE mode

--- dks-host/dks.sh
+++ dks-host/dks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
-export SPRING_PROFILES_ACTIVE=AWS,KMS,INSECURE
+export SPRING_PROFILES_ACTIVE=AWS,KMS,SECURE
 java -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -Ddks.log.directory=/var/log/dks -Ddks.log.level.console=WARN -Ddks.log.level.file=INFO -jar /opt/dks/dks.jar

f63e3ab9024d6d4c18f3259bfcd9cc7ca04d1351
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2019-10-21 12:35:33 +0100

    Fail on first command failure and use env vars for HTTP proxy

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -1,8 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
+set -eEuo pipefail
 
 ## Script to prepare general Dataworks AMI
-
-[ ! -z $HTTP_PROXY ] && pip_http_proxy=" --proxy $(echo $HTTP_PROXY | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g')"
 
 # Install Java
 #sudo yum update -y
@@ -10,7 +9,7 @@
 
 # Install Amazon SSM agent
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-exit 1
+
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator
 acm_cert_helper_version=0.10.0
@@ -23,14 +22,14 @@ sudo yum install -y python-pip
 # of acm-pca-cert-generator to be built and installed
 sudo yum install -y gcc
 sudo yum install -y python-devel
-sudo pip -vvv install$pip_http_proxy https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
+sudo pip -vvv install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
 sudo yum remove -y gcc python-devel
 
 # Adding in netcat and jq for troubleshooting
 sudo yum install -y nmap-ncat jq
 
 # Download & install AWS-CLI
-sudo pip install$pip_http_proxy awscli
+sudo pip install awscli
 
 # rngd is required to generate some entropy without a long wait
 sudo yum install -y rng-tools

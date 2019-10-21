8f07d6533ba1f771d2014da6dc5097423c483509
benjaminsherwood <benjamin.sherwood@dmwgroup.co.uk>
2019-10-21 15:51:00 +0100

    Trying to reduce time of AMI generation
    Compressing yum calls

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -2,10 +2,11 @@
 set -eEuo pipefail
 
 ## Script to prepare general Dataworks AMI
+yum --enablerepo=extras install -y epel-release
 
 # Install Java
 #yum update -y
-#yum install -y java-1.8.0-openjdk-devel
+yum install -y java-1.8.0-openjdk-devel python-pip gcc python-devel nmap-ncat jq rng-tools
 
 # Install Amazon SSM agent
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
@@ -13,24 +14,9 @@ yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/li
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator
 acm_cert_helper_version=0.10.0
-
-# pip is not available in CentOS 7 core repositories there is a requirement to enable EPEL repositories prior
-yum --enablerepo=extras install -y epel-release
-yum install -y python-pip
-
-# gcc and python-devel are required to enable the twofish indirect dependency -
-# of acm-pca-cert-generator to be built and installed
-yum install -y gcc
-yum install -y python-devel
 pip install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
-yum remove -y gcc python-devel
-
-# Adding in netcat and jq for troubleshooting
-yum install -y nmap-ncat jq
-
-# Download & install AWS-CLI
 pip install awscli
 
-# rngd is required to generate some entropy without a long wait
-yum install -y rng-tools
+yum remove -y gcc python-devel
+
 systemctl enable rngd

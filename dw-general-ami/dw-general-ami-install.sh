1c861893dd5e780ea7bae3e008f73b86fa47b325
benjaminsherwood <benjamin.sherwood@dmwgroup.co.uk>
2019-10-21 14:15:52 +0100

    Working on fix to pip

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -4,33 +4,33 @@ set -eEuo pipefail
 ## Script to prepare general Dataworks AMI
 
 # Install Java
-#sudo yum update -y
-#sudo yum install -y java-1.8.0-openjdk-devel
+#yum update -y
+#yum install -y java-1.8.0-openjdk-devel
 
 # Install Amazon SSM agent
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator
 acm_cert_helper_version=0.10.0
 
 # pip is not available in CentOS 7 core repositories there is a requirement to enable EPEL repositories prior
-sudo yum --enablerepo=extras install -y epel-release
-sudo yum install -y python-pip
+yum --enablerepo=extras install -y epel-release
+yum install -y python-pip
 
 # gcc and python-devel are required to enable the twofish indirect dependency -
 # of acm-pca-cert-generator to be built and installed
-sudo yum install -y gcc
-sudo yum install -y python-devel
-sudo pip -vvv install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
-sudo yum remove -y gcc python-devel
+yum install -y gcc
+yum install -y python-devel
+pip -vvv install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
+yum remove -y gcc python-devel
 
 # Adding in netcat and jq for troubleshooting
-sudo yum install -y nmap-ncat jq
+yum install -y nmap-ncat jq
 
 # Download & install AWS-CLI
-sudo pip install awscli
+pip install awscli
 
 # rngd is required to generate some entropy without a long wait
-sudo yum install -y rng-tools
-sudo systemctl enable rngd
+yum install -y rng-tools
+systemctl enable rngd

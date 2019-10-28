75d1dc9ec0a06d178382f1852d2ad18d5ce641b6
Matt Burgess <549318+matthewburgess@users.noreply.github.com>
2019-10-28 11:55:12 +0000

    Explicitly set proxy avoidance settings

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -4,7 +4,13 @@ set -eEuo pipefail
 ## Script to prepare general Dataworks AMI
 
 # Install Amazon SSM agent - download 1st to avoid YUM proxy issues
-curl -O https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+echo "HTTP_PROXY=$HTTP_PROXY"
+echo "HTTPS_PROXY=$HTTPS_PROXY"
+echo "http_proxy=$http_proxy"
+echo "https_proxy=$https_proxy"
+echo "NO_PROXY=$NO_PROXY"
+echo "no_proxy=$no_proxy"
+curl -vvv --noproxy "s3.amazonaws.com" -O https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 yum install -y amazon-ssm-agent.rpm
 rm -f amazon-ssm-agent.rpm
 

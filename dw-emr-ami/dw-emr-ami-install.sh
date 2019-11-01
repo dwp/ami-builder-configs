566a35ae01810b0bb71d2735fbb51f8fea429f80
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2019-11-01 09:29:52 +0000

    Added ami base setup scripts

--- dw-emr-ami/dw-emr-ami-install.sh
+++ dw-emr-ami/dw-emr-ami-install.sh
@@ -14,13 +14,13 @@ echo "no_proxy=$no_proxy"
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 
 # Configure YUM repos to point at fixed mirrors so requests through the proxy will work
-sed -i -e 's/^mirrorlist=/#&/' -e 's/^#baseurl=/baseurl=/' /etc/yum.repos.d/CentOS-Base.repo
-yum --enablerepo=extras install -y epel-release
+# sed -i -e 's/^mirrorlist=/#&/' -e 's/^#baseurl=/baseurl=/' /etc/yum.repos.d/CentOS-Base.repo
+sudo yum-config-manager --enable epel
 sed -i -e 's/^metalink=/#&/' -e 's@^#baseurl=.*@baseurl=http://mirrors.coreix.net/fedora-epel/7/$basearch@' /etc/yum.repos.d/epel.repo
 
 # Install Java
 # yum update -y
-yum install -y java-1.8.0-openjdk-devel python-pip gcc python-devel nmap-ncat jq rng-tools
+yum install -y java-1.8.0-openjdk-devel python-pip gcc python-devel jq rng-tools
 
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator

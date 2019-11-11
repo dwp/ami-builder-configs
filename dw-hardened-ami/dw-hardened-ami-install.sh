8e9022642e3a996f82c8c622e6c1a72b9600a44e
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2019-11-11 11:24:26 +0000

    Moved application configuration to post hardening script e.g. Java 1.8 installation

---
+++ dw-hardened-ami/dw-hardened-ami-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eEu
+
+## Script to prepare general Dataworks AMI
+
+echo "HTTP_PROXY=$HTTP_PROXY"
+echo "HTTPS_PROXY=$HTTPS_PROXY"
+echo "http_proxy=$http_proxy"
+echo "https_proxy=$https_proxy"
+echo "NO_PROXY=$NO_PROXY"
+echo "no_proxy=$no_proxy"
+
+# Install Java
+yum install -y java-1.8.0-openjdk-devel python-pip gcc python-devel jq rng-tools
+yum remove -y gcc python-devel java-1.7.0 --remove-leaves
+chkconfig rngd on

199328277d2ec960d694a905efac9041f9c59f1d
Matt Burgess <549318+matthewburgess@users.noreply.github.com>
2019-11-12 18:16:48 +0000

    Avoid installing Python-2.6

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -19,13 +19,13 @@ yum install -y yum-plugin-remove-with-leaves
 sed -i -e 's/repo_upgrade: security/repo_upgrade: none/' /etc/cloud/cloud.cfg
 yum-config-manager --enable epel
 
-yum install -y python-devel python-pip gcc
+yum install -y python27-devel python27-pip gcc
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator
 acm_cert_helper_version=0.11.0
 pip install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
 pip install --upgrade awscli
 
-yum remove -y gcc python-devel java-1.7.0 --remove-leaves
+yum remove -y gcc python27-devel java-1.7.0 --remove-leaves
 
 echo "export PATH=$PATH:/usr/local/bin" >> /etc/environment

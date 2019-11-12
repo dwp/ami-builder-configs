577b50f9f3030e13a881a78d638ac0296dd5ce82
steveburton4 <Steven.burton@hotmail.co.uk>
2019-11-12 14:00:58 +0000

    Add usr bin to path (#165)
    * Add usr bin to path
    
    * Add botocore

--- dw-general-ami/dw-general-ami-install.sh
+++ dw-general-ami/dw-general-ami-install.sh
@@ -23,7 +23,10 @@ yum install -y python-devel python-pip gcc
 # Install acm cert helper
 acm_cert_helper_repo=acm-pca-cert-generator
 acm_cert_helper_version=0.11.0
+pip install boto3==1.10.15 --upgrade
 pip install https://github.com/dwp/${acm_cert_helper_repo}/releases/download/${acm_cert_helper_version}/acm_cert_helper-${acm_cert_helper_version}.tar.gz
 pip install awscli
 
 yum remove -y gcc python-devel java-1.7.0 --remove-leaves
+
+echo "export PATH=$PATH:/usr/local/bin" >> /etc/environment

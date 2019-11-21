b00feedb92969198443e425b74851af877583077
Benjamin Sherwood <benjamin.sherwood@dwp.gov.uk>
2019-11-21 12:17:31 +0000

    Forgot to remove proxy; typo fix

--- dw-emr-ami/dw-emr-ami-install.sh
+++ dw-emr-ami/dw-emr-ami-install.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -eEu
 
-## Script to prepare general Dataworks AMI
-
-echo "HTTP_PROXY=$HTTP_PROXY"
-echo "HTTPS_PROXY=$HTTPS_PROXY"
-echo "http_proxy=$http_proxy"
-echo "https_proxy=$https_proxy"
-echo "NO_PROXY=$NO_PROXY"
-echo "no_proxy=$no_proxy"
-
 # Make changes to hardened-ami that are required for EMR to work
 
 # Change SELinux config to be permissive
@@ -19,7 +10,7 @@ SELINUXTYPE=targeted
 EOF
 sed -i -e 's/selinux=0/selinux=1 enforcing=0/' /boot/grub/menu.lst
 
-# Relax umask settings adn defaults
+# Relax umask settings and defaults
 sed -i 's/^.*umask 0.*$/umask 022/' /etc/bashrc
 sed -i 's/^.*umask 0.*$/umask 022/' /etc/profile
 sed -i 's/^.*umask 0.*$/umask 022/' /etc/profile.d/*.sh

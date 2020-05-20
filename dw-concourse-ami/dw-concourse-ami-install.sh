12fc64b0eecde96446f2cb9dc17cbbd91582b8f3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-20 09:41:21 +0100

    Added Concourse AMI build script
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

---
+++ dw-concourse-ami/dw-concourse-ami-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eEu
+
+# Make changes to hardened-ami that are required for EMR to work
+
+# Change SELinux config to be permissive
+cat > /etc/selinux/config << EOF
+SELINUX=permissive
+SELINUXTYPE=targeted
+EOF
+sed -i -e 's/selinux=0/selinux=1 enforcing=0/' /boot/grub/menu.lst
+
+# Relax umask settings and defaults
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/bashrc
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
+sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
+
+# Download and Install Concourse
+concourse_tarball="concourse-${concourse_version}-linux-amd64.tgz"
+https_proxy="${https_proxy}" curl -s -L -O https://github.com/concourse/concourse/releases/download/v${concourse_version}/$concourse_tarball
+tar -xzf $concourse_tarball -C /usr/local
+rm $concourse_tarball

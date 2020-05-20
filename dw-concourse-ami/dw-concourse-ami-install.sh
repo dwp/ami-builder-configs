5dca8daad741eb3423b1995d48646c4963323cc9
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-20 15:19:58 +0100

    Added all the proxies
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dw-concourse-ami/dw-concourse-ami-install.sh
+++ dw-concourse-ami/dw-concourse-ami-install.sh
@@ -2,7 +2,12 @@
 set -eEu
 
 # Set Proxy
+echo "HTTP_PROXY=$HTTP_PROXY"
+echo "HTTPS_PROXY=$HTTPS_PROXY"
+echo "http_proxy=$http_proxy"
 echo "https_proxy=$https_proxy"
+echo "NO_PROXY=$NO_PROXY"
+echo "no_proxy=$no_proxy"
 
 # Make changes to hardened-ami that are required for EMR to work
 

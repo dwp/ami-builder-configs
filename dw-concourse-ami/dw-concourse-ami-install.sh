db67898fc60cc4e8ecdec5553f18508c3e65f806
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-20 10:16:01 +0100

    Fixed up proxy and Concourse version
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dw-concourse-ami/dw-concourse-ami-install.sh
+++ dw-concourse-ami/dw-concourse-ami-install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eEu
 
+# Set Proxy
+echo "https_proxy=$https_proxy"
+
 # Make changes to hardened-ami that are required for EMR to work
 
 # Change SELinux config to be permissive
@@ -17,7 +20,8 @@ sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
 sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
 
 # Download and Install Concourse
-concourse_tarball="concourse-${concourse_version}-linux-amd64.tgz"
-https_proxy="${https_proxy}" curl -s -L -O https://github.com/concourse/concourse/releases/download/v${concourse_version}/$concourse_tarball
+concourse_version=v6.1.0
+concourse_tarball="concourse-$concourse_version-linux-amd64.tgz"
+curl -s -L -O https://github.com/concourse/concourse/releases/download/v$concourse_version/$concourse_tarball
 tar -xzf $concourse_tarball -C /usr/local
 rm $concourse_tarball

176399461f3cf4d222505edf31f309bc77a0187d
Liam Rae-McLauchlan <liam.rae-mclauchlan@infinityworks.com>
2020-06-03 13:56:32 +0100

    Add AMI config for ECS

---
+++ dw-ecs-ami/dw-ecs-ami-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -eEu
+
+# Set Proxy
+echo "HTTP_PROXY=$HTTP_PROXY"
+echo "HTTPS_PROXY=$HTTPS_PROXY"
+echo "http_proxy=$http_proxy"
+echo "https_proxy=$https_proxy"
+echo "NO_PROXY=$NO_PROXY"
+echo "no_proxy=$no_proxy"
+
+# Make changes to hardened-ami that are required for ECS to work
+
+# Change SELinux config to be permissive
+cat > /etc/selinux/config << EOF
+SELINUX=permissive
+SELINUXTYPE=targeted
+EOF
+sed -i -e 's/selinux=0/selinux=1 enforcing=0/' /boot/grub/menu.lst
+
+# Relax umask settings and defaults
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/bashrc
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
+sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
+
+# Download and Install ECS Agent
+yum install -y ecs-init
+service docker start
+start ecs

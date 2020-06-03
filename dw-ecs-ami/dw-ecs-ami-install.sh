31497574bd67f830e5a0b7b60fa879edd61e65a1
Liam Rae-McLauchlan <45818099+liamraemclauchlan@users.noreply.github.com>
2020-06-03 15:22:25 +0100

    [DW-4194] Add AMI config for ECS (#221)
    * Add AMI config for ECS

---
+++ dw-ecs-ami/dw-ecs-ami-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eEu
+
+# Set Proxy
+echo "HTTP_PROXY=$HTTP_PROXY"
+echo "HTTPS_PROXY=$HTTPS_PROXY"
+echo "http_proxy=$http_proxy"
+echo "https_proxy=$https_proxy"
+echo "NO_PROXY=$NO_PROXY"
+echo "no_proxy=$no_proxy"
+
+# Make changes to hardened-ami that are required for ECS to work
+
+# Change SELinux config to be permissive
+cat > /etc/selinux/config << EOF
+SELINUX=permissive
+SELINUXTYPE=targeted
+EOF
+sed -i -e 's/selinux=0/selinux=1 enforcing=0/' /boot/grub/menu.lst
+
+# Download and Install ECS Agent
+yum install -y ecs-init
+service docker start
+start ecs

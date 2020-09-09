d2acfec11fa5d724e2d2b5388fe7a142010e7d94
tea-short <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-09-09 15:59:54 +0100

    Restrict SSH

--- dw-al2-hardened-ami/hardening.sh
+++ dw-al2-hardened-ami/hardening.sh
@@ -649,6 +649,7 @@ AllowGroups sshusers
 # OpenSCAP will fail with this cipher set, but ours is more strict
 Ciphers aes256-ctr,aes192-ctr,aes128-ctr
 MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+kexalgorithms curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256,diffie-hellman-group14-sha1
 PermitUserEnvironment no
 SSHCONFIG
 

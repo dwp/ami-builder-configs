c9e4e7c96f3507479a6bc5372cef3c3a24003359
tea-short <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-09-10 09:24:41 +0100

    413 - audit

--- dw-al2-hardened-ami/hardening.sh
+++ dw-al2-hardened-ami/hardening.sh
@@ -378,6 +378,8 @@ done
 echo "#############################################################"
 echo "4.1.3 Ensure auditing for processes that start prior to auditd is enabled"
 sed -i -e '/^-a never,task/ s/$/# /' /etc/audit/audit.rules
+sed -i '/GRUB_CMDLINE_LINUX_DEFAULT.*$/ s/"$/ audit=1"/'  /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 echo "#############################################################"
 echo "4.1.4 Ensure events that modify date and time information are collected"

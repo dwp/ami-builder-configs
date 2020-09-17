2d079fa585fd9a49144401265e95d7d8dd87b656
Dmitri Kovalenko <39998089+tea-short@users.noreply.github.com>
2020-09-17 16:57:58 +0100

    Add AL2 EMR AMI (#229)
    Tested with EMR 5.30.1ю
    
    An EMR cluster using this AMI will require a custom bootstrap action to start the SSM agent as Amazon-provided Systemd unit named "setup-devices" fails to do so. Bootstrap action example:
    
    ```
    #!/bin/bash  
    sudo /bin/systemctl start amazon-ssm-agent
    ```

---
+++ dw-al2-emr-ami/dw-emr-ami-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eEu
+
+# Make changes to hardened-ami that are required for EMR to work
+
+# Change SELinux config to be permissive
+cat > /etc/selinux/config << EOF
+SELINUX=permissive
+SELINUXTYPE=targeted
+EOF
+#sed -i -e 's/selinux=0/selinux=1 enforcing=0/' /boot/grub/menu.lst
+
+# Relax umask settings and defaults
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/bashrc
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile
+sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
+sed -i 's/^umask 027/umask 002/' /etc/init.d/functions

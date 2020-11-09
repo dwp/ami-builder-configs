cf47d65bd69e59a33ac1e6dc795c1ec630c1d5eb
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-11-09 15:03:55 +0000

    debug, comma
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dw-al2-concourse-ami/dw-concourse-ami-install.sh
+++ dw-al2-concourse-ami/dw-concourse-ami-install.sh
@@ -16,8 +16,7 @@ sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
 sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
 
 # Download and Install Concourse
-pwd
-ls
+ls /tmp
 CONCOURSE_TARBALL=$(find /tmp -type f -name *.tgz)
 tar -xzf $CONCOURSE_TARBALL -C /usr/local
 rm $CONCOURSE_TARBALL

5c7b5d08cbe2ef9dfdbcc41ae62f3be229202698
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-03-30 11:58:37 +0100

    Initial commit - Duplicate workflow for ARM AMIs
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

---
+++ dw-al2-hardened-ami-arm/dw-hardened-ami-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eEu
+
+# Install Java
+yum install -y java-1.8.0-openjdk-devel jq rng-tools
+chkconfig rngd on
+
+# Turn down cloud-init logs to prevent false CloudWatch alarms regarding DEBUG logs
+sed -i s/DEBUG/INFO/ /etc/cloud/cloud.cfg.d/05_logging.cfg

4277d41de923734ea5070b2423da39a697bf3ecf
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-03-30 15:32:23 +0100

    pip
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- dw-al2-base-ami/dw-base-ami-install.sh
+++ dw-al2-base-ami/dw-base-ami-install.sh
@@ -46,8 +46,8 @@ yum install -y python3-pip gcc yum-plugin-remove-with-leaves sudo
 echo "Updating pip"
 pip3 install -U pip
 
-pip3 install jinja2
-pip3 install pyyaml
+pip install jinja2
+pip install pyyaml
 
 echo "Install acm cert helper"
 echo "Getting default region"
@@ -62,9 +62,9 @@ if [[ $ARCH != "x86_64" ]]; then
   echo "Installing ARM specific dependencies"
   yum install libffi-devel -y
   echo "Installing acm_cert_helper"
-  pip3 install ./acm_cert_helper-${acm_cert_helper_version}.tar.gz
+  pip install ./acm_cert_helper-${acm_cert_helper_version}.tar.gz
 else
-  pip3 install ./acm_cert_helper-${acm_cert_helper_version}.tar.gz
+  pip install ./acm_cert_helper-${acm_cert_helper_version}.tar.gz
 fi
 
 yum remove -y gcc --remove-leaves

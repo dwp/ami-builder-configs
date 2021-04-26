1cefe5efdcf28f4f6089ba5a160df1fc6af3b930
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-04-26 11:44:32 +0100

    lint
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dw-al2-base-ami/dw-base-ami-install.sh
+++ dw-al2-base-ami/dw-base-ami-install.sh
@@ -54,7 +54,7 @@ acm_cert_helper_version=0.41.0
 echo "Getting cert helper"
 $(which aws) s3 cp s3://$ARTEFACT_BUCKET/acm-pca-cert-generator/acm_cert_helper-${acm_cert_helper_version}.tar.gz .
 
-if [[ $ARCH == "aarch64" ]] then;
+if [[ $ARCH == "aarch64" ]]; then
     
     PATH=$PATH:/usr/local/bin
     

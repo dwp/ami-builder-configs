203cb4dab40338ab04369048ed428c46b0aef4d6
vipinchadha <84083585+vipinchadha@users.noreply.github.com>
2021-05-27 14:35:40 +0100

    DW-6333 - Adding kafka client libs (#279)
    * Adding kafka client libs
    
    * Fixed typo
    
    * Changed download URL
    
    * Changed download URL
    
    * Updated download dir

---
+++ dw-al2-kafka-ami/dw-kafka-ami-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eEu
+
+# Make changes to hardened-ami that are required for ECS to work
+
+# Change SELinux config to be permissive
+cat > /etc/selinux/config << EOF
+SELINUX=permissive
+SELINUXTYPE=targeted
+EOF
+
+# Install Kafka binaries
+mkdir /usr/local/kafka && cd /usr/local/kafka
+curl "http://archive.apache.org/dist/kafka/2.6.2/kafka_2.12-2.6.2.tgz" -o /usr/local/kafka/kafka.tgz
+tar -xvzf /usr/local/kafka/kafka.tgz --strip 1
+
+# Download and Install Kafka Python Client Libs
+pip3 install kafka-python

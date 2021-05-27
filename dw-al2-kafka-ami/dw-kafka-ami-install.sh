9f6d6d825d7f896018173c96d6f7f6c838ff8492
vipinchadha <vipin.chadha@engineering.digital.dwp.gov.uk>
2021-05-27 11:27:05 +0100

    Changed download URL

--- dw-al2-kafka-ami/dw-kafka-ami-install.sh
+++ dw-al2-kafka-ami/dw-kafka-ami-install.sh
@@ -9,12 +9,9 @@ SELINUX=permissive
 SELINUXTYPE=targeted
 EOF
 
-# Install JDK
-yum install -y java-1.8.0-openjdk-devel
-
 # Install Kafka binaries
 mkdir /usr/local/kafka && cd /usr/local/kafka
-curl "https://apache.mirrors.nublue.co.uk/kafka/2.6.2/kafka_2.13-2.6.2.tgz" -o /usr/local/kafka/kafka.tgz
+curl "https://downloads.apache.org/kafka/2.6.2/kafka_2.13-2.6.2.tgz" -o /usr/local/kafka/kafka.tgz
 tar -xvzf ~/Downloads/kafka.tgz --strip 1
 
 # Download and Install Kafka Python Client Libs

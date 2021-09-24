1250b91a0dd20a7095c6032ebd50edd555016c77
Pjay <38280665+gbpjay@users.noreply.github.com>
2021-09-24 16:03:50 +0100

    Update dw-al2-analytical-env-emr-ami-install.sh (#314)
    Updated python code in ami for testing

--- dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
+++ dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
@@ -25,7 +25,9 @@ sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile
 sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
 sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
 
-cat <<EOF > ~/py_requirements.txt
+# building pandas from source requires installing a C compiler so just get a binary.
+cat <<EOF > /tmp/py_requirements.txt
+--only-binary=:pandas:
 nltk==3.6.1
 yake==0.4.7
 spark-nlp==3.0.1
@@ -34,10 +36,15 @@ scikit-spark==0.4.0
 torch==1.8.1
 keras==2.4.3
 scipy==1.6.2
-seaborn==0.11.1
 pandas==1.3.0
 numpy==1.17.3
+seaborn==0.11.1
+statsmodels==0.12.2
+kaleido==0.2.1
+fuzzywuzzy==0.18.0  
+openpyxl==3.0.8
+python-docx==0.8.11
 EOF
 
-pip3 --no-cache-dir install -r ~/py_requirements.txt
-
+sudo -E pip3 install --upgrade pip setuptools || true
+sudo -E python3 -m pip install -r /tmp/py_requirements.txt || true

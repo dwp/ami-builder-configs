8a45957fc910d4c64ead161ae27b1e655e68b828
Pjay <38280665+gbpjay@users.noreply.github.com>
2021-10-01 11:30:32 +0100

    Update dw-al2-analytical-env-emr-ami-install.sh
    DW-6796 - enable debug for python installation

--- dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
+++ dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
@@ -26,34 +26,54 @@ sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile.d/*.sh
 sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
 
 # building pandas from source requires installing a C compiler so just get a binary.
-cat <<EOF > /tmp/debug_py_requirements.txt
+cat <<EOF > /tmp/debug_py_requirements1.txt
 --only-binary=:pandas:
-nltk==3.6.1
-yake==0.4.7
-spark-nlp==3.0.1
+# nltk==3.6.1
+# yake==0.4.7
+# spark-nlp==3.0.1
 scikit-learn==0.24.1
 scikit-spark==0.4.0
-torch==1.8.1
-keras==2.4.3
+# torch==1.8.1
+# keras==2.4.3
 scipy==1.6.2
 pandas==1.3.0
 numpy==1.17.3
 seaborn==0.11.1
 statsmodels==0.12.2
+# kaleido==0.2.1
+# fuzzywuzzy==0.18.0
+# openpyxl==3.0.7
+# python-docx==0.8.11
+# python-Levenshtein==0.12.2
+EOF
+
+
+cat <<EOF > /tmp/debug_py_requirements2.txt
+# --only-binary=:pandas:
+nltk==3.6.1
+yake==0.4.7
+spark-nlp==3.0.1
+# scikit-learn==0.24.1
+# scikit-spark==0.4.0
+torch==1.8.1
+keras==2.4.3
+# scipy==1.6.2
+# pandas==1.3.0
+# numpy==1.17.3
+# seaborn==0.11.1
+# statsmodels==0.12.2
 kaleido==0.2.1
-fuzzywuzzy==0.18.0  
+fuzzywuzzy==0.18.0
 openpyxl==3.0.7
 python-docx==0.8.11
 python-Levenshtein==0.12.2
 EOF
 
-echo -n "Running as: $(whoami)" >> /tmp/pkg_debug.log
-echo -n "Running pip install --upgrade" >> /tmp/pkg_debug.log
-sudo -E pip3 install --upgrade pip setuptools >> /tmp/pkg_debug.log
-echo -n "Running yum install -y python3-devel" >> /tmp/pkg_debug.log
-sudo yum install -y python3-devel >> /tmp/pkg_debug.log
-echo -n "Running python3 -m pip install from req.txt" >> /tmp/pkg_debug.log
-sudo -E python3 -m pip install -r /tmp/debug_py_requirements.txt >> /tmp/pkg_debug.log
-echo -n "Running yum remove" >> /tmp/pkg_debug.log
-sudo yum remove -y python3-devel >> /tmp/pkg_debug.log
+sudo -E pip3 install --upgrade pip setuptools
+sudo yum install -y python3-devel
+echo -e "\n\nRunning python3 -m pip install from req1.txt\n\n" >> /tmp/pkg_debug.log
+sudo -E python3 -m pip install -r /tmp/debug_py_requirements1.txt >> /tmp/pkg_debug.log
+echo -e "\n\nRunning python3 -m pip install from req2.txt\n\n" >> /tmp/pkg_debug.log
+sudo -E python3 -m pip install -r /tmp/debug_py_requirements2.txt >> /tmp/pkg_debug.log
+sudo yum remove -y python3-devel
 echo -n "complete" >> /tmp/pkg_debug.log

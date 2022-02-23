4f334fce38f697c5925fb869092705bc20f2910e
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2022-02-23 08:49:18 +0000

    Dw 8251 (#321)
    * updates required yum pkgs for vulnerability scan
    
    * updates required yum pkgs for vulnerability scan
    
    Co-authored-by: sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>

--- dw-al2-hardened-ami/dw-hardened-ami-install.sh
+++ dw-al2-hardened-ami/dw-hardened-ami-install.sh
@@ -3,6 +3,10 @@ set -eEu
 
 # Install Java
 yum install -y java-1.8.0-openjdk-devel jq rng-tools
+
+# udpate OpenSSH and vim
+yum update -y openssh vim
+
 chkconfig rngd on
 
 # Turn down cloud-init logs to prevent false CloudWatch alarms regarding DEBUG logs

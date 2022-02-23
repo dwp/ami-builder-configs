f560d78acc9b1940de01bbe67cb09146de4f424a
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2022-02-23 08:10:45 +0000

    updates required yum pkgs for vulnerability scan

--- dw-al2-hardened-ami/dw-hardened-ami-install.sh
+++ dw-al2-hardened-ami/dw-hardened-ami-install.sh
@@ -3,6 +3,11 @@ set -eEu
 
 # Install Java
 yum install -y java-1.8.0-openjdk-devel jq rng-tools
+
+# CVE-2021-41617, CVE-2022-0156, CVE-2022-0158, CVE-2022-0213, CVE-2022-0261, CVE-2022-0318, CVE-2022-0351, CVE-2022-0359
+# udpate OpenSSH and vim
+yum update -y openssh vim
+
 chkconfig rngd on
 
 # Turn down cloud-init logs to prevent false CloudWatch alarms regarding DEBUG logs

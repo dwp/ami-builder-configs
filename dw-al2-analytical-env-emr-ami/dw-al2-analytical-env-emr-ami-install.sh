5da1ffacfa67474286b6b9e63c8fc2ee3ef178a9
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2022-11-07 09:01:05 +0000

    collates python packages into single file

--- dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
+++ dw-al2-analytical-env-emr-ami/dw-al2-analytical-env-emr-ami-install.sh
@@ -28,23 +28,26 @@ sed -i 's/^umask 027/umask 002/' /etc/init.d/functions
 # building pandas from source requires installing a C compiler so just get a binary.
 cat <<EOF > /tmp/py_requirements.txt
 --only-binary=:pandas:
+dfply==0.3.3
+dplython==0.0.7
+fuzzywuzzy==0.18.0
+kaleido==0.2.1
+keras==2.4.3
 nltk==3.6.1
-yake==0.4.7
-spark-nlp==3.0.1
+numpy==1.17.3
+openpyxl==3.0.7
+pandas==1.3.0
+PyDriller==2.0
+python-docx==0.8.11
+python-Levenshtein==0.12.2
 scikit-learn==0.24.1
 scikit-spark==0.4.0
-torch==1.8.1
-keras==2.4.3
 scipy==1.6.2
-pandas==1.3.0
-numpy==1.17.3
 seaborn==0.11.1
+spark-nlp==3.0.1
 statsmodels==0.12.2
-kaleido==0.2.1
-fuzzywuzzy==0.18.0
-openpyxl==3.0.7
-python-docx==0.8.11
-# python-Levenshtein==0.12.2 - issue installing here
+torch==1.8.1
+yake==0.4.7
 EOF
 
 sudo -E pip3 install --upgrade pip setuptools

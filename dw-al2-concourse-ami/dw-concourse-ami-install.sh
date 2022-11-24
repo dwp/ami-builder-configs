08c5b3d4d82968dac5c48086a3f246a78477beb5
rorybryett <rory.bryett@dwp.gov.uk>
2022-11-24 14:18:47 +0000

    testing something out

--- dw-al2-concourse-ami/dw-concourse-ami-install.sh
+++ dw-al2-concourse-ami/dw-concourse-ami-install.sh
@@ -9,6 +9,15 @@ SELINUX=permissive
 SELINUXTYPE=targeted
 EOF
 
+cat > /etc/default/grub << EOF
+GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8 net.ifnames=0 biosdevname=0 nvme_core.io_timeout=4294967295 rd.emergency=poweroff"
+GRUB_TIMEOUT=0
+GRUB_DISABLE_RECOVERY="true"
+GRUB_TERMINAL="console serial"
+GRUB_SERIAL_COMMAND="serial --speed=115200"
+GRUB_CMDLINE_LINUX="selinux=0 ipv6.disable=1 audit=1 audit_backlog_limit=8192"
+EOF
+
 # Relax umask settings and defaults
 sed -i 's/^.*umask 0.*$/umask 002/' /etc/bashrc
 sed -i 's/^.*umask 0.*$/umask 002/' /etc/profile

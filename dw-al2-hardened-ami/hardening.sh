b39751b8f2649276d494cb0b28654fe1dae439cd
rorybryett <rory.bryett@dwp.gov.uk>
2022-12-12 17:00:05 +0000

    DTWRKS-360 addition of CIS rule

--- dw-al2-hardened-ami/hardening.sh
+++ dw-al2-hardened-ami/hardening.sh
@@ -327,6 +327,10 @@ echo "Excluded from hardening.sh, added to Userdata in General AMI due to build 
 # yum update -y
 
 echo "#############################################################"
+echo "2.1.1.3 Ensure chrony is configured"
+sed -i 's/OPTIONS.*/OPTIONS="-u chrony -F 2"/g' /etc/sysconfig/chronyd
+
+echo "#############################################################"
 echo "2.2.1.2 Ensure ntp is configured"
 echo "ntp not installed"
 

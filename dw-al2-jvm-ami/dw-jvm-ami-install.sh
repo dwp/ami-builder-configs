bef92dd443d47ba20e8b584776a13a19c3a25fd8
rbryett <13303701+rbryett@users.noreply.github.com>
2022-12-14 14:43:34 +0000

    Dtwrks 360 (#348)
    * DTWRKS-360 make sure we still use standard AWS provided AL2 AMI for Hardened Image Builds
    
    * DTWRKS-360 further corrections
    
    * DTWRKS-360 additional changes to clean up pipeline
    
    * DTWRKS-360 further refactor to differentiate between HCS Backed images and images we create all by ourselves
    
    * DTWRKS-360 further refactor to differentiate between HCS Backed images and images we create all by ourselves
    
    * DTWRKS-360 further refactor to differentiate between HCS Backed images and images we create all by ourselves
    
    * DTWRKS-360 further breaking out of base and hcs-backed amis
    
    * DTWRKS-360 base ami built in pr
    
    * DTWRKS-360
    
    * DTWRKS-360
    
    * DTWRKS-360
    
    * DTWRKS-360
    
    * DTWRKS-360
    
    * DTWRKS-360 this resource isnt used anywhere
    
    * DTWRKS-360:
    
    * DTWRKS-360
    
    * DTWRKS-360 addition of CIS rule
    
    * DTWRKS-360
    
    * DTWRKS-360 config for making JVM ami usable with HCS Backed Images
    
    * DTWRKS-360 using hcs-dualvol
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- dw-al2-jvm-ami/dw-jvm-ami-install.sh
+++ dw-al2-jvm-ami/dw-jvm-ami-install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+set -eEu
 
 # Make changes to hardened-ami that are specific to JVM applications
 echo "Configure AMI changes in ami-builder-configs/dw-al2-jvm-ami"
 
+# Install Java
+yum install -y java-1.8.0-openjdk-devel jq rng-tools
+
+# udpate OpenSSH and vim
+yum update -y openssh vim
+
+chkconfig rngd on
+
+# Turn down cloud-init logs to prevent false CloudWatch alarms regarding DEBUG logs
+sed -i s/DEBUG/INFO/ /etc/cloud/cloud.cfg.d/05_logging.cfg

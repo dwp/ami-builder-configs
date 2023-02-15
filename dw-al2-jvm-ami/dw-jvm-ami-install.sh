4d19ef0d94df98430250e25b44a969c65eeec7d2
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2023-02-15 12:28:15 +0000

    update iptables to default to accept anything that wasn't specifically covered by sg

--- dw-al2-jvm-ami/dw-jvm-ami-install.sh
+++ dw-al2-jvm-ami/dw-jvm-ami-install.sh
@@ -14,3 +14,15 @@ chkconfig rngd on
 
 # Turn down cloud-init logs to prevent false CloudWatch alarms regarding DEBUG logs
 sed -i s/DEBUG/INFO/ /etc/cloud/cloud.cfg.d/05_logging.cfg
+
+# accept anything that wasn't specifically covered
+# temp change until we configure iptables to mirror sg
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+# flushing all rules
+iptables -F
+
+# presisting rules for next boot
+service iptables save

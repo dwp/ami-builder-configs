c9deb02daa0d1eb1d1cc7c20b6428ae0d950e89d
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-04-26 07:20:25 +0100

    fix: fix ecs to specifc ecs-init version

--- dw-al2-ecs-ami/dw-ecs-ami-install.sh
+++ dw-al2-ecs-ami/dw-ecs-ami-install.sh
@@ -37,7 +37,7 @@ echo "baseurl=$(cat mirror.list)" >> /etc/yum.repos.d/amzn2-extras.repo && rm -f
 
 cat /etc/yum.repos.d/amzn2-extras.repo
 
-sudo yum install -y ecs-init jq
+sudo yum install -y ecs-init-1.68.2-1.amzn2.x86_64 jq
 systemctl enable --now ecs amazon-ecs-volume-plugin
 
 # import gpg keys: draios & epel7
